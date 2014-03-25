0d90bcd52475b7e2f472c4e8404ad27f05470e44
nycz <pyntix@gmail.com>
2014-03-25 19:17:48 +0100

    First working commit

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 kalpana-export
 ==============
 
-Export files to different formats
+Export files to different formats. The result is placed in the clipboard.
+
+*Command syntax:* `e<format>[:<chapter>]`
+
+`format` is specified in the config. A format is a list of pairs of strings, with a regex and the replacements. The regex is parsed with the MULTILINE flag, which means that ^ and $ represents the beginning and end of a line.
+
+If the kalpana-chapters plugin is active as well, you can export a single chapter using the `:<chapter>` suffix to the command. `chapter` must be a number of an existing chapter.
+
+All output is stripped of whitespace.
+
+###Example config###
+
+    {
+      "format": {
+        "test": [
+          ["/(.+?)", "<em>\\1</em>"],
+          ["^(.+?)$", "<p>\\1</p>"]
+        ]
+      }
+    }
---
+++ kalpana-export.py
@@ -0,0 +1,58 @@
+import os.path
+import re
+
+from PyQt4 import QtCore, QtGui
+
+from libsyntyche.common import read_json, parse_stylesheet, read_file, make_sure_config_exists
+from pluginlib import GUIPlugin
+
+
+class UserPlugin(GUIPlugin):
+    def __init__(self, objects, get_path):
+        super().__init__(objects, get_path)
+        self.pluginpath = get_path()
+        self.configpath = objects['settings manager'].get_config_directory()
+        self.textarea = objects['textarea']
+        self.chapterplugin = self.get_chapter_plugin(objects['plugins'])
+        self.commands = {
+            'e': (self.export, 'Export the text to custom format')
+        }
+
+    def get_chapter_plugin(self, plugins):
+        for name, p in plugins:
+            if name == 'kalpana-chapters':
+                return p
+
+    def read_config(self):
+        configfile = os.path.join(self.configpath, 'kalpana-export.conf')
+        make_sure_config_exists(configfile, os.path.join(self.pluginpath, 'default_config.json'))
+        self.settings = read_json(configfile)
+
+    def export(self, arg):
+        if not arg:
+            self.error('No export format specified')
+            return
+        # Get a specific chapter to export
+        if re.match(r'.+?:(\d+)$', arg):
+            if not self.chapterplugin:
+                self.error('Chapter plugin not detected')
+                return
+            arg, chapter = arg.rsplit(':',1)
+            if not chapter:
+                self.error('No chapter specified')
+                return
+            text = self.chapterplugin.get_chapter_text(int(chapter))
+            if not text:
+                return
+        # Otherwise the whole text
+        else:
+            text = self.textarea.toPlainText()
+
+        if not arg in self.settings['formats']:
+            self.error('Export format not recognized')
+        else:
+            for rx, rep in self.settings['formats'][arg]:
+                text = re.sub(rx, rep, text, flags=re.MULTILINE)
+            clipboard = QtGui.QApplication.clipboard()
+            clipboard.setText(text.strip('\n\t '))
+            self.print_('Text exported to clipboard')
